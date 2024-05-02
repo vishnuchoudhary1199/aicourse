b4fc486bcf8daa0edd4c47b2c3ff6a7905704c4f
Vishnu <choudharyvishnu1199@gmail.com>
2024-05-02 16:14:43 +0530

    message for commit

---
+++ src/constants.js
@@ -0,0 +1,40 @@
+//BRAND
+export const name = 'AiCourse';
+export const company = 'Spacester';
+export const websiteURL = 'http://localhost:3000';
+export const serverURL = 'http://localhost:5000';
+export const logo = 'https://firebasestorage.googleapis.com/v0/b/india-bd34b.appspot.com/o/artificial-intelligence.png?alt=media&token=2dc0da1c-12d0-4dd2-b908-cb8e57fcf3f0';
+export const razorpayEnabled = true;
+export const paypalEnabled = true;
+
+//PRICING :-
+
+//FREE 
+export const FreeType = 'Free Plan';
+export const FreeCost = 0;
+export const FreeTime = '';
+
+//MONTHLY 
+export const MonthType = 'Monthly Plan';
+export const MonthCost = 9;
+export const MonthTime = 'month';
+
+//YEARLY 
+export const YearType = 'Yearly Plan';
+export const YearCost = 99;
+export const YearTime = 'year';
+
+//TESTIMONIAL
+export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
+export const from = "Anam Meena Sharif";
+export const profession = 'CFO at Spacester';
+export const photoURL = 'https://play-lh.googleusercontent.com/sV_ffBmBJt_je4RZHnfaCfcnL-Hy6C14Iol7H5EMj9fzI2GDOonuojdn5t9p6n9IAX8j';
+
+//PAYPAL
+export const paypalPlanIdOne = "P-1EM732768S920784HMWKW3OA";
+export const paypalPlanIdTwo = "P-8T744865W27080359MWOCE5Q";
+
+//RAZORPAY
+export const razorpayKeyId = "rzp_test_uqALjZHyTyuiOm";
+export const razorpayPlanIdOne = "plan_NMvvtDfznbRp6V";
+export const razorpayPlanIdTwo = "plan_NMRc9HARBQRLWA";
